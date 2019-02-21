7a1e24e11c767d316dfb953579fcd4cdf2507e29
wcolts2000 <wcolts2000@gmail.com>
2019-02-20 16:42:48 -0800

    added a comment inside bubblesort

--- sorting_algos/bubbleSort.js
+++ sorting_algos/bubbleSort.js
@@ -1,25 +1,26 @@
 function bubbleSort(arr) {
-
   function swap(arr, i, j) {
-    let tmp = arr[i]
-    arr[i] = arr[j]
-    arr[j] = tmp
+    let tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
   }
 
   let swapped;
 
-  for(let i = arr.length - 1; i >= 0; i--) {
+  for (let i = arr.length - 1; i >= 0; i--) {
     swapped = false;
-    for(let j = 0; j < i ; j++) {
-      if(arr[j] > arr[j + 1]) {
-        swap(arr, j, j + 1)
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
         swapped = true;
       }
     }
-    if(!swapped) break
+    if (!swapped) break;
   }
 
-  return arr
+  return arr;
 }
 
-console.log(bubbleSort([5, 7, 2, 35, 12, 66]))
+console.log(bubbleSort([5, 7, 2, 35, 12, 66]));
+
+// comment
